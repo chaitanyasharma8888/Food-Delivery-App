d5b732fc3a0810de3c404d63905848d97a435f53
Chaitanya Sharma <chaitanyasharma8888@gmail.com>
2025-06-04 23:53:09 +0530

    card_details_change

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -2,13 +2,8 @@ import { useState } from "react";
 import ItemList from "./ItemList";
 
 const RestaurantCategory = ({ data, showItems, setShowindex, dummy }) => {
-  // console.log("data in accordion", data);
-
-  // const [showItems, setShowItems] = useState(false);//for open and close accordion state
-
   const toggleVisibility = () => {
-    setShowindex(); // Toggle the current state (means flip the current state or change the current state)
-    // console.log("showItems", !showItems); // Note: we log the new state, not current
+    setShowindex();
   };
 
   return (
@@ -18,14 +13,10 @@ const RestaurantCategory = ({ data, showItems, setShowindex, dummy }) => {
           className="bg-blue-200 shadow-sm w-6/12 mx-auto my-0.5 py-4 px-2 flex justify-between hover:cursor-pointer  hover:bg-blue-300 border border-yellow-100 rounded-lg"
           onClick={toggleVisibility}
         >
-          {/*Header section*/}
-
           <span className="font-bold">
             {data?.title}({data?.itemCards.length})
           </span>
           <span>{showItems ? "🔽" : "⏫"}</span>
-
-          {/*Accordion Body section*/}
         </div>
         {showItems && (
           <div className="w-6/12 h-auto mx-auto  bg-blue-100 rounded-xl">
--- src/utils/useOnlineStatus.js
+++ src/utils/useOnlineStatus.js
@@ -1,6 +1,3 @@
-//how to think for creating the hook
-//first think about the contract of hook i.e what is input of hook and what is output/return of hook
-//without turning off internet connection browser gives us ability to check our website how it behave when go offline or simulate a offline experience
 
 import { useState, useEffect } from "react";
 
@@ -10,12 +7,12 @@ const useOnlineStatus = () => {
   useEffect(() => {
     window.addEventListener("offline", () => {
       setOnlineStatus(false);
-    }); //way to add eventListener to webpage via javascript
+    }); 
 
     window.addEventListener("online", () => {
       setOnlineStatus(true);
     });
-  }, []); //executed just once due to empty array of dependency
+  }, []); 
 
   return onlineStatus;
 };
